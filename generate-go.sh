681c21e42361d60832e430fe92aa180c0deb3a7f
Rohit Kumar <rokumar@pivotal.io>
2015-04-13 09:08:01 -0600

    Find path to protoc-gen-gogo more reliably
    * Since $GOPATH can be a composite string, using it as an actual path sometimes does not work

--- generate-go.sh
+++ generate-go.sh
@@ -20,10 +20,10 @@ go get github.com/gogo/protobuf/{proto,protoc-gen-gogo,gogoproto}
 
 pushd events
 mkdir -p $TARGET/events
-protoc --plugin=$GOPATH/bin/protoc-gen-gogo --gogo_out=$TARGET/events *.proto
+protoc --plugin=$(which protoc-gen-gogo) --gogo_out=$TARGET/events *.proto
 popd
 
 pushd control
 mkdir -p $TARGET/control
-protoc --plugin=$GOPATH/bin/protoc-gen-gogo --gogo_out=$TARGET/control *.proto
+protoc --plugin=$(which protoc-gen-gogo) --gogo_out=$TARGET/control *.proto
 popd
